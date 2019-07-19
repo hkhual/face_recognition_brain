36a8bb26af05628af8a514de7705f67fbdf9b1f3
Hau Khual <haukhual08@gmail.com>
2019-07-19 01:15:37 -0700

    deploying

--- src/App.js
+++ src/App.js
@@ -6,18 +6,10 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 
-
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
-
-import Clarifai from 'clarifai';
-
-
-const app = new Clarifai.App({
-  apiKey: 'eb05461834e941dbad59a9e1c1c67d17'
-});
 
 
 const particlesOptions = {
@@ -32,25 +24,26 @@ const particlesOptions = {
   }
 }
 
+const initialState =  {
+  input: '',
+  imageUrl: '',
+  box:{},
+  route: 'signin',
+  isSignedIn: false,
+  user:{
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+    }
+}
 
 class App extends React.Component{
   constructor(){
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box:{},
-      route: 'signin',
-      isSignedIn: false,
-      user:{
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
+    this.state = initialState;
 
-      }
-    }
   }
 
   loadUser = (data) => {
@@ -62,13 +55,6 @@ class App extends React.Component{
       joined: data.joined
     }})
   }
-
-
-  // componentDidMount(){
-  //   fetch('http://localhost:3000/')
-  //   .then(response => response.json())
-  //   .then(console.log);
-  // }
 
   calculateFaceLocation = (data) =>{
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
@@ -97,32 +83,42 @@ class App extends React.Component{
 
   onButtonSubmit = () =>{
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => {
-      if(response){
-        fetch('http://localhost:3000/image',{
-          meethod: 'put',
-          headhers: { 'Content-Type': 'application/json'},
+
+        fetch('https://cryptic-shore-46004.herokuapp.com/imageurl',{
+          method: 'post',
+          headers: { 'Content-Type': 'application/json'},
           body: JSON.stringify({
-            id: this.state.user.id
+            input: this.state.input
           })
         })
         .then(response => response.json())
-        .then(count => {
-          this.setState(Object.assign(this.state.user, {entries: count}))
-        })
-      }
-    
-      
-      this.displayFaceBox(this.calculateFaceLocation(response))
+        .then(response => {
+          if(response){
+            fetch('https://cryptic-shore-46004.herokuapp.com/image',{
+              method:'put',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                id: this.state.user.id
+              })
+            })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+            .catch(console.log);
+          }
+          this.displayFaceBox(this.calculateFaceLocation(response))
     })
+
     .catch(err => console.log(err));
    
   }
 
+
+
   onRouteChange = (route) =>{
     if(route === 'signout'){
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if(route === 'home'){
       this.setState({isSignedIn: true})
     }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -26,7 +26,7 @@ class Register extends React.Component {
 
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {
+        fetch('https://cryptic-shore-46004.herokuapp.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -37,7 +37,7 @@ class Register extends React.Component {
         })
         .then(response => response.json())
         .then(user => {
-            if(user){
+            if(user.id){
                 this.props.loadUser(user);
                 this.props.onRouteChange('home');
             }
